a18f619c788b4f0036335eef1196af8fee03b46b
Šimon Macek <simon.macek@proficio.cz>
2023-12-15 14:12:10 -0500

    TestimonialGrid

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <HeaderMenu />
-    <div class="pt-16 bg-gray-900 min-h-screen">
+    <div class="pt-16 min-h-screen">
       <slot />
     </div>
     <DialogContact />
--- pages/index.vue
+++ pages/index.vue
@@ -5,5 +5,6 @@
     <Jumbotron contact-buton />
     <ProjectFeature class="my-12" />
     <ProjectTeaser class="my-12" />
+    <TestimonialGrid class="my-12" />
   </div>
 </template>
