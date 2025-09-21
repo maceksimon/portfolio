da2c2daa90b2a5b4d81e36759204ce694dc143ac
Šimon Macek <simon.macek@proficio.cz>
2025-09-22 00:00:48 +0200

    update responsivity

--- components/JumbotronCenter.vue
+++ components/JumbotronCenter.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="isolate overflow-hidden pt-14">
     <!-- Animation: Lightbulb -->
-    <NuxtImg src="/image/star.png" alt="" class="absolute w-24 xl:w-64 left-[calc(10%-9px)] top-[calc(15%-22px)] object-contain -z-10 animate-spin-custom xl:top-[calc(15%-72px)] xl:left-[calc(10%-46px)]" />
+    <NuxtImg src="/image/star.png" alt="" class="absolute w-24 xl:w-64 left-[calc(10%-9px)] top-[calc(12%-22px)] object-contain -z-10 animate-spin-custom xl:top-[calc(20%-72px)] xl:left-[calc(10%-46px)]" />
     <!-- Lightbulb -->
-    <NuxtImg src="/image/lightbulb.webp" alt="" class="absolute w-20 xl:w-40 left-[10%] top-[15%] object-contain -z-10" />
+    <NuxtImg src="/image/lightbulb.webp" alt="" class="absolute w-20 xl:w-40 left-[10%] top-[12%] xl:top-[20%] object-contain -z-10" />
 
     <!-- Animation: Hands clap -->
     <!-- Hand 1 (back hand) -->
@@ -32,6 +32,10 @@
           </a>
         </div>
       </div>
+    </div>
+
+    <div aria-hidden="true" class="absolute inset-x-0 -top-8 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80">
+      <div style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)" class="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"></div>
     </div>
   </div>
 </template>
