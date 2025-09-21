c37f38c8468919faa08f3c0f9c9808bd5234d64b
Šimon Macek <simon.macek@proficio.cz>
2025-09-22 00:25:42 +0200

    update page prose

--- pages/ochrana-osobnich-udaju.vue
+++ pages/ochrana-osobnich-udaju.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="p-4 sm:p-8 lg:p-16 mt-12">
-    <div class="prose prose-lg prose-invert mx-auto">
+    <div class="prose prose-lg mx-auto">
       <ContentRenderer :value="post" />
     </div>
   </div>
