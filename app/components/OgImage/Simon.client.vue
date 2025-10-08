11d19cc473241d8ad4538fbc00b5ba01ddc13ca7
Šimon Macek <simon.macek@proficio.cz>
2025-10-08 12:42:38 +0200

    update og image

--- app/components/OgImage/Simon.client.vue
+++ app/components/OgImage/Simon.client.vue
@@ -7,15 +7,34 @@ withDefaults(defineProps<{
 </script>
 
 <template>
-  <div class="h-full w-full flex items-start justify-start border-solid border-blue-500 border-[12px] bg-gray-50">
-    <div class="flex items-start justify-start h-full">
-      <div class="flex flex-col justify-between w-full h-full">
-        <h1 class="text-[80px] p-20 font-black text-left">
+  <div class="h-full w-full flex items-start justify-start border-solid border-blue-500 border-[12px] rounded bg-gray-50">
+    <svg class="absolute top-0 right-0" width="1200" height="675" viewBox="0 0 1200 675" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <g style="mix-blend-mode:overlay" opacity="0.5" filter="url(#filter0_f_448_25)">
+        <circle cx="901.5" cy="45.5" r="199.5" fill="#ff80b5" />
+        <circle cx="600.5" cy="216.5" r="199.5" fill="#9089fc" />
+      </g>
+      <defs>
+        <filter id="filter0_f_448_25" x="-240" y="-374" width="1561" height="1111" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+          <feFlood flood-opacity="0" result="BackgroundImageFix" />
+          <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+          <feGaussianBlur stdDeviation="110" result="effect1_foregroundBlur_448_25" />
+        </filter>
+      </defs>
+    </svg>
+    <div class="flex items-start justify-between h-full">
+      <div class="flex flex-col justify-between">
+        <h1 class="text-[72px] p-20 font-black text-left text-balance">
           {{ title }}
         </h1>
         <p class="text-2xl pb-10 px-20 font-bold mb-0">
           simonjasny.com
         </p>
+      </div>
+      <div class="absolute right-32 bottom-36">
+        <svg xmlns="http://www.w3.org/2000/svg" width="80" height="80" viewBox="0 0 24 24" opacity="0.5"><path fill="none" stroke="#FFBE5E" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12h18m-9-9v18m5-14L7 17M7 7l10 10"/></svg>
+      </div>
+      <div class="absolute right-20 bottom-20">
+        <svg xmlns="http://www.w3.org/2000/svg" width="120" height="120" viewBox="0 0 36 36"><path fill="#333333" d="M18 2.25a11 11 0 0 0-11 11a10.7 10.7 0 0 0 1 4.63a16 16 0 0 0 1.12 1.78a17 17 0 0 1 2 3.47a16.2 16.2 0 0 1 .59 4h2a18.2 18.2 0 0 0-.71-4.69a18.5 18.5 0 0 0-2.22-3.92a16 16 0 0 1-1-1.54A8.64 8.64 0 0 1 9 13.23a9 9 0 0 1 18.07 0a8.64 8.64 0 0 1-.86 3.77a16 16 0 0 1-1 1.54a18.5 18.5 0 0 0-2.21 3.9a18.2 18.2 0 0 0-.71 4.71h2a16.2 16.2 0 0 1 .59-4a17 17 0 0 1 2-3.47A16 16 0 0 0 28 17.86a10.7 10.7 0 0 0 1-4.63A11 11 0 0 0 18 2.25" class="clr-i-outline clr-i-outline-path-1"/><path fill="#FFBE5E" d="M18.63 15.51a.8.8 0 0 0-1.13 0l-3 3l2.86 3.13v5.54H19V21l-2.24-2.45l1.89-1.89a.8.8 0 0 0-.02-1.15" class="clr-i-outline clr-i-outline-path-2"/><path fill="#333333" d="M23.86 29.15H12.11a.8.8 0 1 0 0 1.6h11.75a.8.8 0 0 0 0-1.6" class="clr-i-outline clr-i-outline-path-3"/><path fill="#333333" d="M22 32.15h-8a.8.8 0 1 0 0 1.6h8a.8.8 0 1 0 0-1.6" class="clr-i-outline clr-i-outline-path-4"/><path fill="#FFBE5E" d="m17.32 10.89l-2.73 2.73a.8.8 0 0 0 1.13 1.13L18.45 12a.8.8 0 1 0-1.13-1.13Z" class="clr-i-outline clr-i-outline-path-5"/><path fill="none" d="M0 0h36v36H0z"/></svg>
       </div>
     </div>
   </div>
